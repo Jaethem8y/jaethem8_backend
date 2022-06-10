ce48904c7735134e612bc2e0372a14b6d0eb3775
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-06-10 17:53:37 -0500

    maybe start OO

---
+++ src/main/kotlin/com/jaethem8/jaethem8/model/Content.kt
@@ -0,0 +1,9 @@
+package com.jaethem8.jaethem8.model
+
+abstract class Content(
+    var id:Long?,
+    var location:Int?,
+    var content:String?,
+    var image:String?,
+    var code:String?,
+)
---
+++ src/main/kotlin/com/jaethem8/jaethem8/model/Post.kt
@@ -0,0 +1,8 @@
+package com.jaethem8.jaethem8.model
+
+import java.sql.Date
+
+abstract class Post(
+        var title:String?,
+        var pubDate:Date?,
+)
