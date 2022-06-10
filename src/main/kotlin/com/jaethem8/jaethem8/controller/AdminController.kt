f76b46d6641574301d9b2e65c368cae881a0589d
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-06-09 19:53:26 -0500

    change admin to rest

--- src/main/kotlin/com/jaethem8/jaethem8/controller/AdminController.kt
+++ src/main/kotlin/com/jaethem8/jaethem8/controller/AdminController.kt
@@ -12,9 +12,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 class AdminController(
     private val blogService: BlogService,
